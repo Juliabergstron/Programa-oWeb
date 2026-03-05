16a6ad1cd50032cec2cbd3759f2d356b62c28ef6
Julia Bergstron <julia.bergstron@etec.sp.gov.br>
2026-03-05 17:56:06 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
+<!DOCTYPE html>
 <html>
-
 <head>
-    <title> DOCS </title>
-   <body>
-    <?PHP
-       echo"Olá";
-        echo"Olá";
-         echo"Olá";
-    ?>
-   </body>
+    <title>Aula PHP</title>
 </head>
+   <body>
+     <?PHP
+
+      //DECLARANDO AS VARIAVEIS
+
+      $nome = "Julia "; //Variavel string
+      $sobrenome = "Bergstron"; //Variavel string
+      $idade = 16;//Variavel inteira
+      $nascimentoDia = 27;
+      $nascimentoMes = 8;
+      $nascimentoAno = 2009;
+
+      echo"Nome completo: ";
+      echo $nome;
+      echo $sobrenome;
+      print "<p>";
+
+      echo"Idade: ";
+      echo $idade;
+      print "<p>";
+
+      echo"Nascimento: ";
+      echo $nascimentoDia;
+      echo "/";
+      echo $nascimentoMes;
+      echo "/";
+      echo $nascimentoAno;
+      print "<p>";
+     ?>
+  </body>
 </html>
